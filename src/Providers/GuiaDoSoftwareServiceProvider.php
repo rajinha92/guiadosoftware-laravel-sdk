bdf0aef099e87bd814bfb52fa07ef52186335666
rafael <rafael.conrado.farias@gmail.com>
2017-10-04 15:00:12 -0300

    initial commit

---
+++ src/Providers/GuiaDoSoftwareServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rafael
+ * Date: 04/10/17
+ * Time: 14:46
+ */
+
+namespace GDS\Providers;
+
+
+use GuiaDoSoftwareSdk\Api;
+use Illuminate\Support\ServiceProvider;
+
+class GuiaDoSoftwareServiceProvider extends ServiceProvider
+{
+
+	public function register()
+	{
+		$this->app->singleton(Api::class, function ()
+		{
+			return new Api(config('gds.url'), config('gds.token'));
+		});
+	}
+
+	public function boot()
+	{
+		$this->publishes(
+			['../config/gds.php' => config_path('gds.php'),]
+		);
+	}
+}
---
+++ src/config/gds.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rafael
+ * Date: 04/10/17
+ * Time: 14:46
+ */
+
+return [
+	'url'   => env('GUIA_URL', 'http://guiadosoftware.com'),
+	'token' => env('GUIA_TOKEN', ''), //put here your token
+];
